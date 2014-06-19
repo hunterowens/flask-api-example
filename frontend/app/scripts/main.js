c556c8bd2311a4b723330943b6e7e898ff7a4c21
hunterowens <owens.hunter@gmail.com>
2014-06-19 11:22:53 -0400

    frontend now loads name

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+endpoint = "http://localhost:5000/"
+
+$( document ).ready(function() { 
+$.getJSON(endpoint + "person", function(data) { 
+  console.log(data);
+  //$( "p" ).text( "The DOM is now loaded and can be manipulated." );
+  $( "p" ).text( data.name );
+});
+});
